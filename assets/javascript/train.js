6951c8ce0617f6fa3fcfebeee00ab0176f97eae1
Travis Pickering <Pickerings@Traviss-MacBook-Pro.local>
2016-12-11 15:20:15 -0600

    adding HTML, parts of firebase and JS

---
+++ assets/javascript/train.js
@@ -0,0 +1,14 @@
+
+
+// initialize firebase
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBSXB4UJK288ZOebLUtBBAhZ7y85npFK5o",
+    authDomain: "week-7-train-29341.firebaseapp.com",
+    databaseURL: "https://week-7-train-29341.firebaseio.com",
+    storageBucket: "week-7-train-29341.appspot.com",
+    messagingSenderId: "293918821092"
+  };
+  firebase.initializeApp(config);
+</script>
